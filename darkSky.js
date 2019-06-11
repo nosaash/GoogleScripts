8804a8f1bf8a9a0e4c329853b009d7105f19d769
nosaash <45518828+nosaash@users.noreply.github.com>
2019-06-10 23:36:45 -0400

    Update darkSky.js

--- darkSky.js
+++ darkSky.js
@@ -1,5 +1,13 @@
+/*
+This fetch the current weather data from Dark Sky API to a Google Spreadsheet.
+Need to obtain own API key for the request. Free tier can do 1000 requests per day. 
+*/
+
 function fetchDarkSky() {
+  //the exclude is to shorten the total API response. 
   var url = 'https://api.darksky.net/forecast/API_KEY/40.444546,%20-79.958903?exclude=minutely,hourly,daily,alerts,flags';
+  
+  
   var response = UrlFetchApp.fetch(url);
   var json = response.getContentText();
   var data = JSON.parse(json);
@@ -7,34 +15,36 @@ function fetchDarkSky() {
 
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getSheets()[0];
+  
+  //Details of the response: https://darksky.net/dev/docs#response-format
   var values = [time_now_string, 
-                data.currently.summary,
-                data.currently.precipProbability,
-                data.currently.visibility,
-                data.currently.windGust,
+                data.currently.summary, //cloudy, drizzling, etc. 
+                data.currently.precipProbability, 
+                data.currently.visibility, //miles
+                data.currently.windGust, //mph
                 data.currently.precipIntensity,
                 data.currently.precipIntensityError,
                 data.currently.icon,
                 data.currently.cloudCover,
-                data.currently.windBearing,
-                (data.currently.apparentTemperature - 32)*5/9,
+                data.currently.windBearing, //wind direction, 0 deg is north. Progressing clockwise. 
+                (data.currently.apparentTemperature - 32)*5/9, //apparent (feels like) temperature converted to Celcius
                 data.currently.pressure,
-                (data.currently.dewPoint - 32)*5/9,
+                (data.currently.dewPoint - 32)*5/9, //dew point temperature converted to Celcius
                 data.currently.ozone,
                 data.currently.nearestStorm,
                 data.currently.Distance,
                 data.currently.precipType,
-                (data.currently.temperature - 32)*5/9,
-                data.currently.humidity,
+                (data.currently.temperature - 32)*5/9, //temperature converted to Celcius
+                data.currently.humidity, 
                 data.currently.time,
                 data.currently.windSpeed,
                 data.currently.uvIndex,]
   
   sheet.appendRow(values);
   
-  //if (data.currently.icon == "rain"){
-  //    MailApp.sendEmail("XXXXX@gmail.com", "DarkSky " + time_now_string + " " + data.currently.icon, JSON.stringify(data.currently));
-  //};
+  if (data.currently.icon == "rain"){
+      MailApp.sendEmail("XXXXX@gmail.com", "DarkSky " + time_now_string + " " + data.currently.icon, JSON.stringify(data.currently));
+  };
   
 }
 
